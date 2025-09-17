5046376e3cb58209629f83787b7f11235dbc0cd2
LovkiiZerg <viktor.may23@gmail.com>
2025-09-17 21:06:13 +0300

    Add files via upload

---
+++ laba1/laba1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+class ATC {
+private:
+	int users = 50;
+	float price = 499.99;
+	std::string adress = "Pushkina";
+public:
+	void print()
+	{
+		setlocale(LC_ALL, "rus");
+		std::cout << "Абонентская плата всех клиентов: " << users * price << std::endl;
+	}
+};
+
+int main()
+{
+	ATC dom;
+	dom.print();
+}
